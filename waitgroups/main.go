d4a2873f034349135713e91d2244a0d73d4cd4bc
Ian Howell <ian.howell0@gmail.com>
2025-03-07 14:46:52 +0000

    tiny tweak to waitgroups example

--- waitgroups/main.go
+++ waitgroups/main.go
@@ -31,29 +31,27 @@ func main() {
 	fmt.Println("Initial goroutine finished!")
 
 	// Spin up a handful of goroutines
-	for range 10 { // bonus! check out the new for-range loop introduced in go1.22
+	for i := range 10 { // bonus! check out the new for-range loop introduced in go1.22
 		// Add the goroutines to the waitgroup
 		wg.Add(1)
 
 		// Sleep for a random amount of time. Concurrently.
-		go func() {
-			sleepRandom(5000)
+		go func(i int) {
+			// Generate a random duration between 0 and 5 seconds
+			d := time.Duration(rand.Intn(5000)) * time.Millisecond
+
+			// Sleep!
+			fmt.Printf("Goroutine %d:   Sleeping for %v\n", i, d)
+			time.Sleep(d)
 
 			// Mark the goroutine as done
+			fmt.Printf("Goroutine %d:   Woke up after %v\n", i, d)
 			wg.Done()
-		}()
+		}(i)
 	}
 
 	// wg.Wait will block until *all* goroutines have called Done
 	fmt.Println("Waiting for goroutines to finish...")
 	wg.Wait()
 	fmt.Println("All goroutines finished!")
-}
-
-// sleepRandom sleeps a random number of milliseconds between 0 and n
-func sleepRandom(n int) {
-	d := time.Duration(rand.Intn(n)) * time.Millisecond
-	fmt.Println("Sleeping for", d)
-	time.Sleep(d)
-	fmt.Println("Woke up after", d)
 }
