990fdf61aaefec524a57f6205d1aab39d3e7701d
Ian Howell <ian.howell0@gmail.com>
2025-03-07 13:57:58 +0000

    Clean up the intro example

--- goroutines/main.go
+++ goroutines/main.go
@@ -1,35 +1,38 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	/*
-		In order to run a function asynchronously in golang the `go`
-		keyword can be used.  The go runtime is complex and does all
-		sorts of magic with OS threads etc, more on that later.
-	*/
+	// In order to run a function asynchronously in golang the `go` keyword can be used. The go runtime is
+	// complex and does all sorts of magic with OS threads etc.
 
-	// This function is blocking, it will block the program for 5 seconds
-	// as its blocking the main thread
-	synchronous()
-
-	// This function will not block, the go runtime will multiplex it
-	// However, because it is not blocking it will not prevent main() from
-	// exiting.  main will exit instantly, regardless of the sleep.
-	// more on other concurrency primities to handle these cases later.
+	// This function will not block; the Go runtime will multiplex it. However, because it is not
+	// blocking, it will not prevent main from exiting. main will exit instantly, regardless of what the
+	// function is doing.
 	go asynchronous()
 
-	/*
-		Here we technically, leaked a goroutine but the program will terminate in 5 seconds:
+	// This function is blocking, it will block the program for 5 seconds as its blocking the main thread
+	fmt.Println("This function is blocking!")
+	synchronous()
 
-		go run goroutines/main.go  0.05s user 0.06s system 1% cpu 5.059 total
-	*/
+	// We're technically leaking a goroutine here, but since this is not a long running program, the Go
+	// runtime will clean up after us when main exits.
 }
 
 func synchronous() {
+	fmt.Println("synchronous: Sleeping for 5 seconds...")
 	time.Sleep(5 * time.Second)
+	fmt.Println("synchronous: Done!")
 }
 
 func asynchronous() {
-	time.Sleep(1 * time.Hour)
+	for {
+		fmt.Println("asynchronous: Sleeping for 1 second...")
+		time.Sleep(1 * time.Second)
+	}
+	// Unreachable...
+	fmt.Println("asynchronous: Done!")
 }
