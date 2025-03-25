5aa7da48cf3189aeacb81fc6837c296f4150eccd
Ian Howell <ian.howell0@gmail.com>
2025-03-25 23:44:38 +0000

    Add more detail to the select example

--- select/main.go
+++ select/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
@@ -13,13 +17,19 @@ func main() {
 	go odds(nums, oddCh)
 	go evens(nums, evenCh)
 
-	for range nums {
+	numRecieved := 0
+	for numRecieved < len(nums) {
 		// The select will block until one of the channels recieves a value
 		select {
 		case num := <-oddCh:
 			fmt.Printf("%d is odd!\n", num)
+			numRecieved++
 		case num := <-evenCh:
 			fmt.Printf("%d is even!\n", num)
+			numRecieved++
+			// default:
+			// 	fmt.Println("No values are ready")
+			// 	time.Sleep(1 * time.Second)
 		}
 	}
 }
@@ -29,6 +39,7 @@ func odds(nums []int, c chan int) {
 		if num%2 == 1 {
 			c <- num
 		}
+		randSleep()
 	}
 }
 
@@ -37,5 +48,11 @@ func evens(nums []int, c chan int) {
 		if num%2 == 0 {
 			c <- num
 		}
+		randSleep()
 	}
+}
+
+func randSleep() {
+	d := time.Duration(rand.Intn(100))
+	time.Sleep(d * time.Millisecond)
 }
